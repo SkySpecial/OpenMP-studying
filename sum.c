8a4b7e0eb1af1447bcb8f4c99b8c3b99f56b68c5
SkySpecial <dmitrii.cherepanov@phystech.edu>
2015-10-10 01:40:29 +0300

    Why segm fault?

--- sum.c
+++ sum.c
@@ -5,12 +5,13 @@ int main()
 {
 	int num_of_iter;
 	float result = 0;
-	int i;
+	int i = 1;
 
 	scanf("%d", num_of_iter);
 	#pragma omp parallel for
-	for( i = 0; i < num_of_iter; i++)
-		result += 1.0/i;// . показывает, что деление всё-таки плавающее?
+	for( i = 1; i <= num_of_iter; i++)
+		result += 1.0/i;// . показывает, что деление всё-таки плавающе?
 	printf("%f\nThe number of terms:%d\n", result, num_of_iter);
 	return 0;
 }
+
