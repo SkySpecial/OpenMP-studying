5d614e52fab75253b48f11ed83f5646e1251767b
SkySpecial <dmitrii.cherepanov@phystech.edu>
2015-10-10 01:29:49 +0300

    Update hello.c
    Jeder thread ist in Ordnung!

--- hello.c
+++ hello.c
@@ -3,12 +3,21 @@
 
 int main()
 {
-	int i = 0;
-	#pragma omp parallel
-	{
-		printf("Hello!I am thread %d\n", omp_get_thread_num() );
-		i++;
+        int i = 0;
+        int indicator = 0;
+        #pragma omp parallel shared(indicator)
+        {
+		while ( indicator != 32)
+		{
+                	if (omp_get_thread_num() == indicator)
+                	{
+                        	printf("Hello!I am thread %d\n", omp_get_thread_num() );
+                        	indicator++;
+				i++;
+               		}
+        	}
 	}
-	printf("n = %d\n", i);
-	return 0;
-}	
+        printf("n = %d\n", i);
+        return 0;
+}
+	
