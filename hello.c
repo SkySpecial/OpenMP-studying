f86afe1f1c61f402ee1ce600ea6d4f22eb6b965a
SkySpecial <dmitrii.cherepanov@phystech.edu>
2015-10-09 23:44:28 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,8 +6,8 @@ int main()
 	int i = 0;
 	#pragma omp parallel
 	{
-	printf("Hello!I am thread %d\n", omp_get_thread_num() );
-	i++;
+		printf("Hello!I am thread %d\n", omp_get_thread_num() );
+		i++;
 	}
 	printf("n = %d\n", i);
 	return 0;
