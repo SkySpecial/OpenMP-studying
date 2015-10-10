800f55272b3e72f21f1c25110f23d9998adbe70f
SkySpecial <dmitrii.cherepanov@phystech.edu>
2015-10-10 19:49:50 +0300

    Update sum.c

--- sum.c
+++ sum.c
@@ -7,10 +7,10 @@ int main()
 	float result = 0;
 	int i = 1;
 
-	scanf("%d", num_of_iter);
+	scanf("%d", &num_of_iter);
 	#pragma omp parallel for
 	for( i = 1; i <= num_of_iter; i++)
-		result += 1.0/i;// . показывает, что деление всё-таки плавающе?
+		result += 1.0/i;
 	printf("%f\nThe number of terms:%d\n", result, num_of_iter);
 	return 0;
 }
